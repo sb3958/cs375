a28b7d3fe5eff35bdec11e9674bf3dee1ceea039
Sharelle Boddie <sb3958@drexel.edu>
2021-05-22 13:30:50 -0400

    Center Page

--- public_html/search.js
+++ public_html/search.js
@@ -4,11 +4,19 @@ function getUsername(){
     return user;
 }
 
+function removeRows(element) {
+    while (element.hasChildNodes()) {
+		element.lastChild.remove();
+	}
+}
+
 let button = document.getElementById("search");
 button.addEventListener("click", function(){
     let username = getUsername();
     let url = `/search?username=${username}`
     fetch(url).then (function (response){
-
+        return response.json().then (function (data){
+            
+        })
     })
 })
